e5c6fc7d0d2603fc65f3be08b15306bda6f3e80d
abel-wxm <abel.wxm@gmail.com>
2026-02-16 22:14:03 +0800

    Update main_prototype.cpp

--- src/main_prototype.cpp
+++ src/main_prototype.cpp
@@ -1,186 +1,118 @@
 // ==========================================
-// VERSION: 2026-02-16_15-40 (Button Fix)
+// VERSION: V1.0_2026-02-16_18-00 (UI Overlay)
 // ==========================================
 #define WIN32_LEAN_AND_MEAN
-// 再次定义，双重保险
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
 #include <windows.h>
+#include <dwmapi.h>
 #include <string>
-#include <iomanip>
-#include <sstream>
+#include <vector>
 #include "RecorderEngine.hpp"
 
 retrorec::RecorderEngine g_engine;
+HWND hToolbar, hOverlay;
 
-#define IDC_BTN_START 101
-#define IDC_BTN_PAUSE 102
-#define IDC_BTN_STOP  103
-#define IDC_BTN_PEN   104
-#define IDC_BTN_MOSAIC 105
-#define IDC_BTN_CLEAR 106
+#define IDC_START 1
+#define IDC_STOP 2
+#define IDC_PEN 3
+#define IDC_MOSAIC 4
 
-HWND hBtnStart, hBtnPause, hBtnStop, hBtnPen, hBtnMosaic, hBtnClear;
-
-std::string formatTime(double s) {
-    int total = (int)s;
-    int m = total / 60;
-    int sec = total % 60;
-    std::stringstream ss;
-    ss << std::setw(2) << std::setfill('0') << m << ":" << std::setw(2) << std::setfill('0') << sec;
-    return ss.str();
-}
-
-void UpdateUI(HWND hWnd) {
-    bool rec = g_engine.isRecording();
-    bool paused = g_engine.isPaused();
-    bool pen = g_engine.isPaintMode();
-    bool mosaic = g_engine.isMosaicMode();
-
-    EnableWindow(hBtnStart, !rec);
-    EnableWindow(hBtnPause, rec);
-    EnableWindow(hBtnStop, rec);
-    EnableWindow(hBtnPen, rec);
-    EnableWindow(hBtnMosaic, rec);
-    EnableWindow(hBtnClear, rec);
-
-    SetWindowTextA(hBtnPause, paused ? "Resume" : "Pause");
-    SetWindowTextA(hBtnPen, pen ? "[ PEN ON ]" : "Pen");
-    SetWindowTextA(hBtnMosaic, mosaic ? "[ MOSAIC ]" : "Mosaic");
-
-    InvalidateRect(hWnd, nullptr, FALSE);
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK OverlayProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
-    case WM_CREATE: {
-        int x = 10; int w = 80; int h = 30; int gap = 5;
-        
-        hBtnStart = CreateWindowA("BUTTON", "Record", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 
-            x, 40, w, h, hWnd, (HMENU)IDC_BTN_START, ((LPCREATESTRUCT)lParam)->hInstance, NULL); x += w + gap;
-        
-        hBtnPause = CreateWindowA("BUTTON", "Pause", WS_TABSTOP | WS_VISIBLE | WS_CHILD, 
-            x, 40, w, h, hWnd, (HMENU)IDC_BTN_PAUSE, ((LPCREATESTRUCT)lParam)->hInstance, NULL); x += w + gap;
-
-        hBtnStop = CreateWindowA("BUTTON", "Stop", WS_TABSTOP | WS_VISIBLE | WS_CHILD, 
-            x, 40, w, h, hWnd, (HMENU)IDC_BTN_STOP, ((LPCREATESTRUCT)lParam)->hInstance, NULL); x += w + gap + 15;
-
-        hBtnPen = CreateWindowA("BUTTON", "Pen", WS_TABSTOP | WS_VISIBLE | WS_CHILD, 
-            x, 40, w, h, hWnd, (HMENU)IDC_BTN_PEN, ((LPCREATESTRUCT)lParam)->hInstance, NULL); x += w + gap;
-
-        hBtnMosaic = CreateWindowA("BUTTON", "Mosaic", WS_TABSTOP | WS_VISIBLE | WS_CHILD, 
-            x, 40, w, h, hWnd, (HMENU)IDC_BTN_MOSAIC, ((LPCREATESTRUCT)lParam)->hInstance, NULL); x += w + gap;
-            
-        hBtnClear = CreateWindowA("BUTTON", "Clear", WS_TABSTOP | WS_VISIBLE | WS_CHILD, 
-            x, 40, w, h, hWnd, (HMENU)IDC_BTN_CLEAR, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
-
-        UpdateUI(hWnd);
-        SetTimer(hWnd, 1, 1000, NULL);
-    } break;
-
-    case WM_COMMAND: {
-        int id = LOWORD(wParam);
-        switch (id) {
-        case IDC_BTN_START:
-            if (g_engine.startRecording()) UpdateUI(hWnd);
-            break;
-        case IDC_BTN_PAUSE:
-            if (g_engine.isPaused()) g_engine.resumeRecording();
-            else g_engine.pauseRecording();
-            UpdateUI(hWnd);
-            break;
-        case IDC_BTN_STOP:
-            g_engine.stopRecording();
-            UpdateUI(hWnd);
-            MessageBoxA(hWnd, "Saved Successfully!", "RetroRec", MB_OK);
-            break;
-        case IDC_BTN_PEN:
-            g_engine.togglePaintMode();
-            UpdateUI(hWnd);
-            break;
-        case IDC_BTN_MOSAIC:
-            g_engine.toggleMosaicMode();
-            UpdateUI(hWnd);
-            break;
-        case IDC_BTN_CLEAR:
-            g_engine.clearEffects();
-            break;
-        }
-    } break;
-
     case WM_PAINT: {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hWnd, &ps);
-        RECT rect;
-        GetClientRect(hWnd, &rect);
-        
-        SetBkMode(hdc, TRANSPARENT);
-        HFONT hFont = CreateFont(20, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, TEXT("Segoe UI"));
-        SelectObject(hdc, hFont);
+        auto strokes = g_engine.getStrokes();
+        auto zones = g_engine.getMosaicZones();
 
-        std::string status;
-        if (g_engine.isRecording()) {
-            status = g_engine.isPaused() ? "PAUSED" : "REC: " + formatTime(g_engine.getDuration());
-            SetTextColor(hdc, RGB(255, 0, 0));
-        } else {
-            status = "RetroRec v1.0 - Ready";
-            SetTextColor(hdc, RGB(0, 0, 0));
+        HPEN hPen = CreatePen(PS_SOLID, 3, RGB(255, 0, 0));
+        HPEN hOld = (HPEN)SelectObject(hdc, hPen);
+        for (const auto& p : strokes) {
+            MoveToEx(hdc, p.x, p.y, NULL);
+            LineTo(hdc, p.x+1, p.y+1);
         }
+        SelectObject(hdc, hOld);
+        DeleteObject(hPen);
 
-        // 使用 TextOutA 并强转 int，绝对安全
-        TextOutA(hdc, 15, 10, status.c_str(), (int)status.length());
-        
-        std::string hint = "";
-        if (g_engine.isPaintMode()) hint = " [PEN ON] Draw Red Lines";
-        else if (g_engine.isMosaicMode()) hint = " [MOSAIC ON]";
-        
-        if (!hint.empty()) {
-            SetTextColor(hdc, RGB(0, 0, 200));
-            TextOutA(hdc, 220, 10, hint.c_str(), (int)hint.length());
+        HPEN hMPen = CreatePen(PS_DASH, 1, RGB(0, 0, 255));
+        HBRUSH hNull = (HBRUSH)GetStockObject(NULL_BRUSH);
+        SelectObject(hdc, hMPen);
+        SelectObject(hdc, hNull);
+        for (const auto& r : zones) {
+            Rectangle(hdc, r.x, r.y, r.x + r.w, r.y + r.h);
         }
-
-        DeleteObject(hFont);
+        DeleteObject(hMPen);
         EndPaint(hWnd, &ps);
     } break;
-
-    case WM_TIMER:
-        if (g_engine.isRecording()) InvalidateRect(hWnd, nullptr, FALSE);
-        break;
-
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
+    case WM_NCHITTEST: {
+        if (g_engine.isPaintMode() || g_engine.isMosaicMode()) return HTCLIENT;
+        return HTTRANSPARENT; 
+    } break;
+    case WM_LBUTTONDOWN:
+    case WM_MOUSEMOVE: {
+        if (wParam & MK_LBUTTON) {
+            POINT pt; GetCursorPos(&pt);
+            if (g_engine.isPaintMode()) g_engine.addStroke(pt.x, pt.y);
+            else if (g_engine.isMosaicMode()) g_engine.addMosaic(pt.x-10, pt.y-10, 20, 20);
+        }
+    } break;
+    default: return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
 }
 
-int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR, int nCmdShow) {
-    WNDCLASSEXA wcex = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, WndProc, 0, 0, hInstance, LoadIcon(nullptr, IDI_APPLICATION), LoadCursor(nullptr, IDC_ARROW), (HBRUSH)(COLOR_WINDOW + 1), nullptr, "RetroRecClass", nullptr };
-    RegisterClassExA(&wcex);
+LRESULT CALLBACK ToolbarProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_CREATE:
+        CreateWindow("BUTTON", "Record", WS_VISIBLE | WS_CHILD, 10, 10, 60, 30, hWnd, (HMENU)IDC_START, 0, 0);
+        CreateWindow("BUTTON", "Stop", WS_VISIBLE | WS_CHILD, 80, 10, 60, 30, hWnd, (HMENU)IDC_STOP, 0, 0);
+        CreateWindow("BUTTON", "Pen", WS_VISIBLE | WS_CHILD, 150, 10, 60, 30, hWnd, (HMENU)IDC_PEN, 0, 0);
+        CreateWindow("BUTTON", "Mosaic", WS_VISIBLE | WS_CHILD, 220, 10, 60, 30, hWnd, (HMENU)IDC_MOSAIC, 0, 0);
+        SetTimer(hWnd, 1, 30, NULL); 
+        break;
+    case WM_COMMAND:
+        switch (LOWORD(wParam)) {
+        case IDC_START: g_engine.startRecording(); break;
+        case IDC_STOP: g_engine.stopRecording(); MessageBox(hWnd, "Saved!", "RetroRec", MB_OK); break;
+        case IDC_PEN: g_engine.togglePaintMode(); break;
+        case IDC_MOSAIC: g_engine.toggleMosaicMode(); break;
+        }
+        break;
+    case WM_TIMER:
+        InvalidateRect(hOverlay, NULL, TRUE);
+        break;
+    case WM_DESTROY: PostQuitMessage(0); break;
+    default: return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
 
-    int screenW = GetSystemMetrics(SM_CXSCREEN);
-    HWND hWnd = CreateWindowA("RetroRecClass", "RetroRec Toolbar", WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX, 
-        (screenW - 650) / 2, 0, 650, 120, nullptr, nullptr, hInstance, nullptr);
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR, int) {
+    WNDCLASSEX wc1 = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, ToolbarProc, 0, 0, hInstance, 0, LoadCursor(0, IDC_ARROW), (HBRUSH)(COLOR_WINDOW + 1), 0, "ToolbarClass", 0 };
+    RegisterClassEx(&wc1);
+    WNDCLASSEX wc2 = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, OverlayProc, 0, 0, hInstance, 0, LoadCursor(0, IDC_ARROW), 0, 0, "OverlayClass", 0 };
+    RegisterClassEx(&wc2);
 
-    if (!hWnd) return 0;
+    hToolbar = CreateWindowEx(WS_EX_TOPMOST, "ToolbarClass", "RetroRec V1.0", WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX, 100, 100, 320, 100, 0, 0, hInstance, 0);
+    ShowWindow(hToolbar, SW_SHOW);
+
+    int sw = GetSystemMetrics(SM_CXSCREEN);
+    int sh = GetSystemMetrics(SM_CYSCREEN);
+    hOverlay = CreateWindowEx(WS_EX_TOPMOST | WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW, 
+        "OverlayClass", "", WS_POPUP, 0, 0, sw, sh, hToolbar, 0, hInstance, 0);
+    
+    SetLayeredWindowAttributes(hOverlay, 0, 0, LWA_COLORKEY);
+    ShowWindow(hOverlay, SW_SHOW);
 
     g_engine.initialize();
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
-    SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 
-    MSG msg = {0};
-    while (msg.message != WM_QUIT) {
-        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        } else {
-            g_engine.captureFrame();
-        }
+    MSG msg;
+    while (GetMessage(&msg, 0, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+        g_engine.captureFrame();
     }
     return (int)msg.wParam;
 }
